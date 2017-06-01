f688e8bf5a8879cf9b72007613cda7ae8a223507
Adam Dickison <adamd@teamcornett.com>
2017-06-01 08:51:35 -0400

    Guided burger building with Papa Style easter egg. 104 tests passing

--- aw_burger_builder.js
+++ aw_burger_builder.js
@@ -3,18 +3,19 @@ var _ = require('lodash');
 var Burger = require('./burger');
 var RandomHelper = require('./random_helper');
 
+var AWBurger = new Burger();
+var randomHelper = new RandomHelper();
+
 function AWBurgerBuilder() {};
 
 //#PROPERTIES
-AWBurgerBuilder.prototype.AWBurger = new Burger();
-AWBurgerBuilder.prototype.randomHelper = new RandomHelper();
 AWBurgerBuilder.prototype.burgerLayers = 3; //cheese, bacon, make it a double
-AWBurgerBuilder.prototype.toppingLayers = AWBurgerBuilder.AWBurger.availableToppings().length;
-AWBurgerBuilder.prototype.sauceLayers = AWBurgerBuilder.AWBurger.availableSauces().length
+AWBurgerBuilder.prototype.toppingLayers = AWBurger.availableToppings().length;
+AWBurgerBuilder.prototype.sauceLayers = AWBurger.availableSauces().length
 
 //#METHODS
 AWBurgerBuilder.prototype.formatBurger = function () {
-    var burger = this.AWBurger.toJson();
+    var burger = AWBurger.toJson();
 
     var toppings = '';
     if (this.formatAdditions(burger['toppings']) != '') {
@@ -30,13 +31,24 @@ AWBurgerBuilder.prototype.formatBurger = function () {
         });
     }
 
-    return _.template('A ${double}${bacon}${cheese}burger${toppings}${sauces}')({
+
+    var tmpl = _.template('A ${double}${bacon}${cheese}burger${toppings}${sauces}')({
         double: burger['double'] ? 'double ' : null,
         bacon: burger['bacon'] ? 'bacon ' : null,
         cheese: burger['cheese'] ? 'cheese' : null,
         toppings: toppings,
         sauces: sauces
     });
+
+    if(tmpl.indexOf('topped with Lettuce, Pickles, Tomato, and Onion with Papa Sauce') !== -1){
+        return _.template('A ${double}${bacon}${cheese}burger Papa Style')({
+            double: burger['double'] ? 'double ' : null,
+            bacon: burger['bacon'] ? 'bacon ' : null,
+            cheese: burger['cheese'] ? 'cheese' : null
+        });
+    } else {
+        return tmpl;
+    }
 }
 
 AWBurgerBuilder.prototype.formatAdditions = function (additions) {
@@ -61,32 +73,32 @@ AWBurgerBuilder.prototype.formatAdditions = function (additions) {
 };
 
 AWBurgerBuilder.prototype.randomBurger = function () {
-    this.AWBurger.reset();
-    if (this.randomHelper.FiftyFifty()) {
+    AWBurger.reset();
+    if (randomHelper.FiftyFifty()) {
         this.addCheese();
     }
 
-    if (this.randomHelper.FiftyFifty()) {
+    if (randomHelper.FiftyFifty()) {
         this.addBacon();
     }
 
-    if (this.randomHelper.FiftyFifty()) {
+    if (randomHelper.FiftyFifty()) {
         this.makeItADouble();
     }
 
-    var numberOfSauces = this.randomHelper.RandomNumber(0, this.AWBurger.availableSauces().length);
+    var numberOfSauces = randomHelper.RandomNumber(0, AWBurger.availableSauces().length);
     if (numberOfSauces > 0) {
-        var randomSauces = this.randomHelper.NonRepeatingRandom(0, this.AWBurger.availableSauces().length - 1, numberOfSauces);
+        var randomSauces = randomHelper.NonRepeatingRandom(0, AWBurger.availableSauces().length - 1, numberOfSauces);
         for (var index in randomSauces) {
-            this.addSauce(this.AWBurger.availableSauces()[index]);
+            this.addSauce(AWBurger.availableSauces()[index]);
         }
     }
 
-    var numberOfToppings = this.randomHelper.RandomNumber(0, this.AWBurger.availableToppings().length);
+    var numberOfToppings = randomHelper.RandomNumber(0, AWBurger.availableToppings().length);
     if (numberOfToppings > 0) {
-        var randomToppings = this.randomHelper.NonRepeatingRandom(0, this.AWBurger.availableToppings().length - 1, numberOfToppings);
+        var randomToppings = randomHelper.NonRepeatingRandom(0, AWBurger.availableToppings().length - 1, numberOfToppings);
         for (var index in randomToppings) {
-            this.addTopping(this.AWBurger.availableToppings()[index]);
+            this.addTopping(AWBurger.availableToppings()[index]);
         }
     }
 
@@ -94,162 +106,174 @@ AWBurgerBuilder.prototype.randomBurger = function () {
 };
 
 AWBurgerBuilder.prototype.NextBurgerLayer = function () {
-    if (this.burgerLayers != 3) {
+    
+     if (this.burgerLayers != 0) {
         switch (this.burgerLayers) {
             case 1:
-                break;
+                return "Should I make it a double?";
             case 2:
-                break;
+                return "How about bacon?";
             case 3:
-                break;
+                return "Let's build a burger! First things first. Would you like cheese?";
             default:
-                //something bad happened
+                return "Something went terribly wrong! Our gravitational reversal of meat technician is on the fritz. Please check back later while we reboot her."
         }
     } else {
-        if (this.toppingLayers != this.AWBurger.availableToppings().length) {
+        if (this.toppingLayers != 0) {
             switch (this.toppingLayers) {
                 case 1:
-                    break;
+                    return "Finally, would you like grilled onions on your burger?"
                 case 2:
-                    break;
+                    return "Grilled mushrooms?"
                 case 3:
-                    break;
+                    return "Would you like to spice it up a bit with Jalapeño Peppers?";
                 case 4:
-                    break;
+                    return "Do you like onion on your burger?";
                 case 5:
-                    break;
+                    return "How about a slice of juicy tomato?";
                 case 6:
-                    break;
+                    return "Some pickles?";
                 case 7:
-                    break;
+                    return "Now onto the toppings! Would you like lettuce?";
                 default:
-                    //something bad happened
+                    return "Something went terribly wrong! Our gravitational reversal of meat technician is on the fritz. Please check back later while we reboot him."
             }
         } else {
-            if (this.sauceLayers != this.AWBurger.availableSauces().length) {
+            if (this.sauceLayers != 0) {
                 switch (this.sauceLayers) {
                     case 1:
-                        break;
+                        return "We're just about done! How about some Mustard?";
                     case 2:
-                        break;
+                        return "Would you like Ketchup?";
                     case 3:
-                        break;
+                        return "Do you like Mayo on your burger?";
                     case 4:
-                        break;
+                        return "Spicy Ketchup?";
                     case 5:
-                        break;
+                        return "What about some of our spicy Papa Sauce?";
                     case 6:
-                        break;
+                        return "No burger would be complete without some sauce. How about some of our famous Papa Sauce?";
                     default:
-                        //something bad happened
+                        return "Something went terribly wrong! Our gravitational reversal of meat technician is on the fritz. Please check back later while we reboot her."
                 }
             } else {
-                //all done!
-            }
+                //we're all done
+                return false;
+            } 
         }
     }
 };
 
 AWBurgerBuilder.prototype.AddBurgerLayer = function (add) {
-if (this.burgerLayers != 3) {
-        switch (this.burgerLayers) {
-            case 1:
-                this.burgerLayers--;
-                break;
-            case 2:
-                if(add){
-                    this.AWBurger.addBacon();
-                }
-                this.burgerLayers--;
-                break;
-            case 3:
-                if(add){
-                    this.AWBurger.addCheese();
-                }
-                this.burgerLayers--;
-                break;
-            default:
-                //something bad happened
-        }
-    } else {
-        if (this.toppingLayers != this.AWBurger.availableToppings().length) {
-            switch (this.toppingLayers) {
+    if (this.burgerLayers != 0) {
+        if (add) {
+            switch (this.burgerLayers) {
                 case 1:
-                    this.toppingLayers--;
+                    this.makeItADouble();
                     break;
                 case 2:
-                    this.toppingLayers--;
+                    AWBurger.addBacon();
                     break;
                 case 3:
-                    this.toppingLayers--;
-                    break;
-                case 4:
-                    this.toppingLayers--;
-                    break;
-                case 5:
-                    this.toppingLayers--;
-                    break;
-                case 6:
-                    this.toppingLayers--;
-                    break;
-                case 7:
-                    this.toppingLayers--;
+                    AWBurger.addCheese();
                     break;
                 default:
-                    //something bad happened
-            }
-        } else {
-            if (this.sauceLayers != this.AWBurger.availableSauces().length) {
-                switch (this.sauceLayers) {
+                    return "Something went terribly wrong! Our gravitational reversal of meat technician is on the fritz. Please check back later while we reboot him."
+            } 
+        }
+        this.burgerLayers--;
+
+    } else {
+        if (this.toppingLayers != 0) {
+            if(add){
+                switch (this.toppingLayers) {
                     case 1:
-                        this.sauceLayers--;
+                        this.addTopping('Grilled Onions');
                         break;
                     case 2:
-                        this.sauceLayers--;
+                        this.addTopping('Grilled Mushrooms');
                         break;
                     case 3:
-                        this.sauceLayers--;
+                        this.addTopping('Jalapeño Peppers');
                         break;
                     case 4:
-                        this.sauceLayers--;
+                        this.addTopping('Onion');
                         break;
                     case 5:
-                        this.sauceLayers--;
+                        this.addTopping('Tomato');
                         break;
                     case 6:
-                        this.sauceLayers--;
+                        this.addTopping('Pickles');
+                        break;
+                    case 7:
+                        this.addTopping('Lettuce');
                         break;
                     default:
-                        //something bad happened
+                        return "Something went terribly wrong! Our gravitational reversal of meat technician is on the fritz. Please check back later while we reboot her."
                 }
-            } else {
-                //all done!
             }
+            this.toppingLayers--;
+        } else {
+            if (this.sauceLayers != 0) {
+                if(add){
+                    switch (this.sauceLayers) {
+                        case 1:
+                            this.addSauce('Mustard');
+                            break;
+                        case 2:
+                            this.addSauce('Ketchup');
+                            break;
+                        case 3:
+                            this.addSauce('Mayo');
+                            break;
+                        case 4:
+                            this.addSauce('Spicy Ketchup');
+                            break;
+                        case 5:
+                            this.addSauce('Spicy Papa');
+                            break;
+                        case 6:
+                            this.addSauce('Papa Sauce');
+                            break;
+                        default:
+                            return "Something went terribly wrong! Our gravitational reversal of meat technician is on the fritz. Please check back later while we reboot him."
+                    }
+
+                }
+            } 
+            this.sauceLayers--;
         }
     }
+    //check if all layers have been filled if so restart loop return burger
 };
 
 //#WRAPPER FUNCTIONS FOR BURGER
 AWBurgerBuilder.prototype.addCheese = function () {
-    this.AWBurger.addCheese();
+    AWBurger.addCheese();
 };
 
 AWBurgerBuilder.prototype.addBacon = function () {
-    this.AWBurger.addBacon();
+    AWBurger.addBacon();
 };
 
 AWBurgerBuilder.prototype.makeItADouble = function () {
-    this.AWBurger.makeItADouble();
+    AWBurger.makeItADouble();
 };
 
 AWBurgerBuilder.prototype.addSauce = function (sauce) {
-    this.AWBurger.addSauce(sauce);
+    AWBurger.addSauce(sauce);
 };
 
 AWBurgerBuilder.prototype.addTopping = function (topping) {
-    this.AWBurger.addTopping(topping);
+    AWBurger.addTopping(topping);
 };
 
+AWBurgerBuilder.prototype.reset = function(){
+    AWBurger.reset();
+    this.burgerLayers = 3; //cheese, bacon, make it a double
+    this.toppingLayers = AWBurger.availableToppings().length;
+    this.sauceLayers = AWBurger.availableSauces().length
+}
 
 
 
--- test/test_03_aw_burger_builder.js
+++ test/test_03_aw_burger_builder.js
@@ -18,4 +18,221 @@ describe('AWBurgerBuilder', function(){
             });
         });
     });
+
+    describe("#BuildABurger", function(){
+        context("Guided Burger Building (Everything)", function(){
+            it('Ask cheese, add cheese', function(){
+                subject.reset();
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(true);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask bacon, add bacon', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(true);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask ask double, add double', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(true);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask lettuce, add lettuce', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(true);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask pickle, add pickle', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(true);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask tomato, add tomato', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(true);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask onion, add onion', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(true);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask jalapeño peppers, add jalapeño peppers', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(true);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask mushrooms, add mushrooms', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(true);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask grilled onion, add grilled onion', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(true);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask papa sauce, add papa sauce', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(true);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask spicy papa, add spicy papa', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(true);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask spicy ketchup, add spicy ketchup', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(true);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask mayo, add mayo', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(true);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask ketchup, add ketchup', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(true);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask mustard, add mustard', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(true);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask layer, return burger', function(){
+                if(subject.NextBurgerLayer() === false){
+                    console.log("You ordered a " + subject.formatBurger());
+                }
+                
+                expect(true).to.be.bool;
+            });
+        });
+    });
+
+    describe("#BuildABurger", function(){
+        context("Guided Burger Builder (Papa Style)", function(){
+            it('Ask cheese, add cheese', function(){
+                subject.reset();
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(true);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask bacon, add bacon', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(true);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask ask double, no double', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(false);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask lettuce, add lettuce', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(true);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask pickle, add pickle', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(true);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask tomato, add tomato', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(true);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask onion, add onion', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(true);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask jalapeño peppers, no jalapeño peppers', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(false);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask mushrooms, no mushrooms', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(false);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask grilled onion, no grilled onion', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(false);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask papa sauce, add papa sauce', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(true);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask spicy papa, no spicy papa', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(false);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask spicy ketchup, no spicy ketchup', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(false);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask mayo, no mayo', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(false);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask ketchup, no ketchup', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(false);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask mustard, no mustard', function(){
+                console.log(subject.NextBurgerLayer());
+                subject.AddBurgerLayer(false);
+                console.log(subject.formatBurger());
+                expect(true).to.be.bool;
+            });
+            it('Ask layer, return burger', function(){
+                if(subject.NextBurgerLayer() === false){
+                    console.log("You ordered a " + subject.formatBurger());
+                }
+                expect(true).to.be.bool;
+            });
+        });
+    });
 });
